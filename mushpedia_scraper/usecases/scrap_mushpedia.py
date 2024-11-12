450cd442ba209ec59a0b2098b5ddc21f07ce2f50
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2024-11-12 19:06:37 +0100

    refactor: Move multithreading logic to a decorator

--- mushpedia_scraper/usecases/scrap_mushpedia.py
+++ mushpedia_scraper/usecases/scrap_mushpedia.py
@@ -1,9 +1,7 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from multiprocessing import cpu_count
-
 from bs4 import BeautifulSoup
 
 from mushpedia_scraper.ports.page_reader import PageReader
+from mushpedia_scraper.thread_pool_decorator import thread_pool
 
 
 class ScrapeMushpedia:
@@ -12,7 +10,8 @@ class ScrapeMushpedia:
     def __init__(self, page_reader: PageReader) -> None:
         self.page_reader = page_reader
 
-    def execute(self, mushpedia_links: list[str], workers: int = -1) -> list[str]:
+    @thread_pool
+    def execute(self, mushpedia_links: list[str]) -> list[str]:
         """Execute the use case on the given Mushpedia links.
 
         Args:
@@ -21,25 +20,8 @@ class ScrapeMushpedia:
         Returns:
             list[str]: A list of scrapped Mushpedia articles in HTML format.
         """
-        results = []
-        with ThreadPoolExecutor(max_workers=self._get_workers(workers)) as executor:
-            # Submit all scraping tasks
-            future_to_url = {executor.submit(self._scrap_page, url): url for url in mushpedia_links}
-
-            # Collect results as they complete
-            for future in as_completed(future_to_url):
-                try:
-                    result = future.result()
-                    results.append(result)
-                except Exception as exc:
-                    url = future_to_url[future]
-                    print(f"Scraping generated an exception for {url}: {exc}")
-
-        return results
+        return mushpedia_links
 
     def _scrap_page(self, page_reader_link: str) -> str:
         page_reader = BeautifulSoup(self.page_reader.get(page_reader_link), "html.parser")
         return page_reader.prettify()
-
-    def _get_workers(self, workers: int) -> int:
-        return cpu_count() if workers == -1 else min(workers, cpu_count())
